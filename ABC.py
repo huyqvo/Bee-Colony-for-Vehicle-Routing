e04f4e50ca7c5c2bee2d22dd4bbd7d3956a4c842
dantevergilkat <dantevergilkat@gmail.com>
2019-12-03 11:03:38 +0700

    Update: neighborOps

--- ABC.py
+++ ABC.py
@@ -4,33 +4,31 @@ import random
 
 def swapReverse_neighborOps(arr, maxLen):
     while True:
-        s1 = random.randint(0, arr.shape[0])
-        l1 = random.randint(2, maxLen)
-        if 0 in arr[s1:s1+l1]:
+        s1 = random.randint(0, arr.shape[0] - 4)
+        e1 = random.randint(s1+1, arr.shape[0] - 3)                                                                                                                      
+        if 0 in arr[s1:e1+1]:
             break
     while True:
-        while True:
-            s2 = random.randint(0, arr.shape[0])
-            if s2 > s1 + l1:
-                break 
-        l2 = random.randint(2, maxLen)
-        if 0 in arr[s2:s2+l2]:
+        s2 = random.randint(e1+1, arr.shape[0] - 2)
+        e2 = random.randint(s2+1, arr.shape[0] - 1)
+        if 0 in arr[s2:e2+1]:
             break
 
     prob1 = np.random.uniform()
     prob2 = np.random.uniform()
-    arr1 = np.copy(arr[s1:s1+l1])
+    arr1 = np.copy(arr[s1:e1+1])
     if prob1 > 0.5:
         arr1 = np.flip(arr1)
-    arr2 = np.copy(arr[s2:s2+l2])
+    arr2 = np.copy(arr[s2:e2+1])
     if prob2 > 0.5:
         arr2 = np.flip(arr2)
 
     new_arr = np.empty(0)
     new_arr = np.concatenate((new_arr, np.copy(arr[0:s1])))
     new_arr = np.concatenate((new_arr, arr2))
-    new_arr = np.concatenate((new_arr, arr[s1+l1:s2]))
-    new_arr = np.concatenate((new_arr, arr[s2+l2:]))
+    new_arr = np.concatenate((new_arr, arr[e1+1:s2]))
+    new_arr = np.concatenate((new_arr, arr1))
+    new_arr = np.concatenate((new_arr, arr[e2+1:]))
 
     return new_arr
     
